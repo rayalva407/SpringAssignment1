31d0faa9c64a685c06ace0b6045323bcc8849928
rayalva407 <rayalva407@gmail.com>
2022-04-26 12:07:27 -0600

    Dependency Injection

--- src/main/java/org/genspark/App.java
+++ src/main/java/org/genspark/App.java
@@ -1,13 +1,20 @@
 package org.genspark;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    //Use XML to configure the spring container to achieve DI for the above code,
+    // later use Java Annotations and Java Configuration methods.
+    public static void main( String[] args ) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
+
+        Student student = (Student) context.getBean("student");
+
+        System.out.println(student);
+        System.out.println("ID: " + student.getId());
+        System.out.println("Name: " + student.getName());
+        System.out.println("Phone: " + student.getPh());
+        System.out.println("Address: " + student.getAdd());
     }
 }
